fb932ac2a92ae4371f1563cc03b2eed50cc2f617
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2023-12-18 22:32:18 +0200

    update: add message to indicate that there are no tasks

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -33,41 +33,45 @@ const Tasks = () => {
         </div>
 
         <div style={{ overflow: "auto" }}>
-          <table className="tasks__table">
-            <thead>
-              <tr>
-                <td>ID</td>
-                <td>Title</td>
-                <td>Creator</td>
-                <td>Asigned to</td>
-                <td>Actions</td>
-              </tr>
-            </thead>
-            <tbody>
-              {tasks.map((task) => (
-                <tr key={task.id}>
-                  <td>{task.id}</td>
-                  <td>{task.title}</td>
-                  <td>{task.createdBy}</td>
-                  <td>{task.assignedTo}</td>
-                  <td className="tasks__table-actions">
-                    <GMButton
-                      text="update"
-                      onClick={() => handleUpdateTask(task.id)}
-                      image={editIcon}
-                      colour={"green"}
-                    />
-                    <GMButton
-                      text="delete"
-                      onClick={() => dispatch(removeTask(task.id))}
-                      image={deleteIcon}
-                      colour="red"
-                    />
-                  </td>
+          {tasks.length > 0 ? (
+            <table className="tasks__table">
+              <thead>
+                <tr>
+                  <td>ID</td>
+                  <td>Title</td>
+                  <td>Creator</td>
+                  <td>Asigned to</td>
+                  <td>Actions</td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {tasks.map((task) => (
+                  <tr key={task.id}>
+                    <td>{task.id}</td>
+                    <td>{task.title}</td>
+                    <td>{task.createdBy}</td>
+                    <td>{task.assignedTo}</td>
+                    <td className="tasks__table-actions">
+                      <GMButton
+                        text="update"
+                        onClick={() => handleUpdateTask(task.id)}
+                        image={editIcon}
+                        colour={"green"}
+                      />
+                      <GMButton
+                        text="delete"
+                        onClick={() => dispatch(removeTask(task.id))}
+                        image={deleteIcon}
+                        colour="red"
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p className="tasks__empty">No Tasks yet</p>
+          )}
         </div>
       </div>
 
