8949200176973198da0d970b32c358178e2ecc65
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2023-12-18 17:19:21 +0200

    update: add delete all task functionality

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,14 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import GMButton from "../GMButton/GMButton";
+import { deleteAllTasks } from "../../redux/actions";
+
 import "./Sidebar.css";
 
 const Sidebar = () => {
+  const dispatch = useDispatch();
   const tasks = useSelector((state) => state.tasks);
+
   return (
     <aside className="sidebar">
       {/* TODO */}
@@ -13,7 +17,10 @@ const Sidebar = () => {
       <p className="sidebar__tasks">Number of tasks ({tasks.length})</p>
 
       {/* TODO */}
-      <GMButton text="Delete All Tasks" />
+      <GMButton
+        text="Delete All Tasks"
+        onClick={() => dispatch(deleteAllTasks())}
+      />
     </aside>
   );
 };
