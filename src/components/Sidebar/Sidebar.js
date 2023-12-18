8f9cedb21ac69b7e3bdb6c8aeb036a6aeb41fa80
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2023-12-18 13:50:00 +0200

    add base sidebar

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      {/* TODO */}
+      <p className="sidebar__tasks">Current use</p>
+
+      {/* TODO */}
+      <p className="sidebar__tasks">Number of tasks (25)</p>
+
+      {/* TODO */}
+      <button className="sidebar__btn">Delete All Tasks</button>
+    </aside>
+  );
+};
+
+export default Sidebar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 // import "./Home.css";
 
@@ -8,6 +9,7 @@ const Home = () => {
     <div className="home">
       <Header />
       <Hero />
+      <Sidebar />
     </div>
   );
 };
