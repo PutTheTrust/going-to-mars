1371c28a6597c226cfa43295df6be88df4df40d5
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2023-12-18 16:58:02 +0200

    update: sidebar to show how many tasks are available

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,16 @@
+import { useSelector } from "react-redux";
+
 import GMButton from "../GMButton/GMButton";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+  const tasks = useSelector((state) => state.tasks);
   return (
     <aside className="sidebar">
       {/* TODO */}
       <p className="sidebar__tasks">Current use</p>
 
-      {/* TODO */}
-      <p className="sidebar__tasks">Number of tasks (25)</p>
+      <p className="sidebar__tasks">Number of tasks ({tasks.length})</p>
 
       {/* TODO */}
       <GMButton text="Delete All Tasks" />
