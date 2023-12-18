e5afa731c538196103b3c6ed8476810c7348a553
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2023-12-18 18:57:14 +0200

    update: ading task functionality

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,15 +1,41 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+
 import GMButton from "../GMButton/GMButton";
 import GMInput from "../GMInput/GMInput";
 
 import close from "../../assets/icon-close.svg";
 import "./Modal.css";
+import { addTask } from "../../redux/actions";
 
 const Modal = ({ setIsOpen }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [creator, setCreator] = useState("");
   const [assigned, setAssigned] = useState("");
+  // used to keep track of id being added
+  const [tmpId, setTmpId] = useState(21);
+
+  const dispatch = useDispatch();
+
+  const handleAddNewTask = () => {
+    if (!title || !description || !creator || !assigned) {
+      alert("please fill in all inputs");
+      return;
+    }
+
+    dispatch(
+      addTask({
+        id: tmpId,
+        title,
+        description,
+        createdBy: creator,
+        assignedTo: assigned,
+      })
+    );
+    setTmpId(tmpId + 1);
+  };
+
   return (
     <div className="modal">
       <div className="modal__content">
@@ -44,7 +70,7 @@ const Modal = ({ setIsOpen }) => {
         />
 
         <div style={{ marginTop: "16px" }}>
-          <GMButton text="Create Task" />
+          <GMButton text="Create Task" onClick={() => handleAddNewTask()} />
         </div>
       </div>
     </div>
