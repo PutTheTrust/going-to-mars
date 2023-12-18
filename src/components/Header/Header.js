0413f48b8ccca5d6c32e81934d31a6813d0dedf8
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2023-12-18 13:10:54 +0200

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const index = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        Red Frontier: Conquering the Martian Mystique
+      </h1>
+    </header>
+  );
+};
+
+export default index;
